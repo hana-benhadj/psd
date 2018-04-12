860ce9ab97da267b440a15bb13ae0844872470fb
Hana Ben Hadj Abdallah <h.benhadj@root.sa>
2018-04-12 09:13:49 +0100

    Add project files.

---
+++ psd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(psd.Startup))]
+namespace psd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
